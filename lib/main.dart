e6c738f70d71d43ad72d5bad82e5b68acf8bc656
nukopy <nukopy@gmail.com>
2023-03-21 03:45:49 +0900

    feat: DONE chap02-01

--- lib/main.dart
+++ lib/main.dart
@@ -58,58 +58,66 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      print("Hello World");
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          title: Row(
+        children: const [
+          Icon(Icons.create),
+          Text("初めてのタイトル"),
+        ],
+      )),
+      body: Column(
+        children: [
+          const Text("Hello World"),
+          const Text("ハローワールド"),
+          TextButton(
+            onPressed: () => {print("ボタンが押されたよ")},
+            child: const Text("ボタン"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Icon(
+                Icons.favorite,
+                color: Colors.pink,
+                size: 24.0,
+              ),
+              Icon(
+                Icons.audiotrack,
+                color: Colors.green,
+                size: 32.5,
+              ),
+              Icon(
+                Icons.beach_access,
+                color: Colors.blue,
+                size: 45.0,
+              ),
+            ],
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => {print("押したね?")},
+        child: const Icon(Icons.timer),
+      ),
+      drawer: Drawer(
+        child: Column(children: const [
+          Text("Drawer"),
+          AboutDialog(),
+        ]),
+      ),
+      endDrawer: Drawer(
+        child: Column(children: const [
+          Text("End Drawer"),
+          AboutDialog(),
+        ]),
+      ),
     );
   }
 }
